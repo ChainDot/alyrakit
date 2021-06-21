4c963ab61aad0c10d993726a01133e5a2374115d
ChainDot <crypto.dot@protonmail.com>
2021-06-21 19:00:00 +0200

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,73 @@
+import { Button, IconButton } from "@chakra-ui/button";
+import { HamburgerIcon, MoonIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/layout";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+
 const Navigation = () => {
   return (
-    <nav>
-      <a href="/">AlyraKit</a>
-      <ul>
-        <li>
-          <a href="/#sample">Sample</a>
-        </li>
-        <li>
-          <a href="/#pricing">Pricing</a>
-        </li>
-        <li>
-          <a href="/#buy-now">Buy now</a>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+    <Box position="fixed" w="100%" py="4" zIndex="100" bg="whiteAlpha.800">
+      <Container
+        as="nav"
+        display="flex"
+        justifyContent="space-between"
+        align="center"
+        maxW="container.lg"
+      >
+        <Link href="/" textDecoration="none">
+          <Heading size="lg">AlyraKit</Heading>
+        </Link>
+        <List>
+          <Flex justify="space-around" align="center">
+            <ListItem mx="4">
+              <Link href="/#sample">
+                <Text fontSize="lg" textTransform="uppercase">
+                  Sample
+                </Text>
+              </Link>
+            </ListItem>
+            <ListItem mx="4">
+              <Link href="/#pricing" textTransform="uppercase">
+                Pricing
+              </Link>
+            </ListItem>
+            <ListItem mx="4">
+              <Link href="/#buy-now" textTransform="uppercase">
+                Buy now
+              </Link>
+            </ListItem>
 
-export default Navigation
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+                mx="2"
+              />
+              <MenuList>
+                <MenuItem>New Tab</MenuItem>
+                <MenuItem command="⌘N">New Window</MenuItem>
+                <MenuItem>Open Closed Tab</MenuItem>
+                <MenuItem>Open File...</MenuItem>
+              </MenuList>
+            </Menu>
+            <Button variant="outline">
+              <MoonIcon w="4" h="4" />
+            </Button>
+          </Flex>
+        </List>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navigation;
